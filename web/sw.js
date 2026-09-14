d6ed748fe82f53f92ec938078ff789af37d68fad
dennysivan2892-ai <dennysivan2892@gmail.com>
2026-09-14 13:33:02 +0200

    Actualizar caché para check-in diario y avatares

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE='vicbangfit-v4-nutrition-weights';
+const CACHE='vicbangfit-v5-checkin-day-admin-avatars';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 
 self.addEventListener('install',event=>{
